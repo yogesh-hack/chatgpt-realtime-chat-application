ee1324b835e389fd9ac6c398706157a509412f53
Yogesh Baghel <yogesh0909baghel@gmail.com>
2024-04-04 13:31:23 +0530

    Adding files

---
+++ server/index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import OpenAI from "openai";
+import openAiRoutes from "./routes/openai.js";
+import authRoutes from "./routes/auth.js";
+
+/* CONFIGURATIONS */
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+/* OPEN AI CONFIGURATION */
+export const openai = new OpenAI({
+  apiKey: process.env['OPEN_API_KEY'],
+});
+
+/* ROUTES */
+app.use("/openai", openAiRoutes);
+app.use("/auth", authRoutes);
+
+/* SERVER SETUP */
+const PORT = process.env.PORT || 9000;
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
---
+++ server/routes/openai.js
@@ -0,0 +1,106 @@
+import express from "express";
+import axios from "axios";
+import dotenv from "dotenv";
+import { openai } from "../index.js";
+
+dotenv.config();
+const router = express.Router();
+
+router.post("/text", async (req, res) => {
+  try {
+    const { text, activeChatId } = req.body;
+    console.log("text", text);
+
+    const params = {
+      messages: [{ role: 'user', content: text }],
+      model: 'gpt-3.5-turbo',
+      prompt: text,
+      temperature: 0.5,
+      max_tokens: 2048,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0,
+    };
+    const response = await openai.chat.completions.create(params);
+    res.json(response.data);
+
+    await axios.post(
+      `https://api.chatengine.io/chats/${activeChatId}/messages/`,
+      { text: response.data.choices[0].text },
+      {
+        headers: {
+          "Project-ID": process.env.PROJECT_ID,
+          "User-Name": process.env.BOT_USER_NAME,
+          "User-Secret": process.env.BOT_USER_SECRET,
+        },
+      }
+    );
+
+    res.status(200).json({ text: response.data.choices[0].text });
+  } catch (error) {
+    console.error("error", error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.post("/code", async (req, res) => {
+  try {
+    const { text, activeChatId } = req.body;
+
+    const params = {
+      messages: [{ role: 'user', content: text }],
+      model: 'code-davinci-002',
+      prompt: text,
+      temperature: 0.5,
+      max_tokens: 2048,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0,
+    };
+    const response = await openai.chat.completions.create(params);
+    res.json(response.data);
+
+    await axios.post(
+      `https://api.chatengine.io/chats/${activeChatId}/messages/`,
+      { text: response.data.choices[0].text },
+      {
+        headers: {
+          "Project-ID": process.env.PROJECT_ID,
+          "User-Name": process.env.BOT_USER_NAME,
+          "User-Secret": process.env.BOT_USER_SECRET,
+        },
+      }
+    );
+
+    res.status(200).json({ text: response.data.choices[0].text });
+  } catch (error) {
+    console.error("error", error.response.data.error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.post("/assist", async (req, res) => {
+  try {
+    const { text } = req.body;
+    
+    const params = {
+      messages: [{ role: 'user', content: text }],
+      model: 'text-davinci-003',
+      prompt: `Finish my thought: ${text}`,
+      temperature: 0.5,
+      max_tokens: 1024,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0,
+    };
+    const response = await openai.chat.completions.create(params);
+    res.json(response.data);
+
+    res.status(200).json({ text: response.data.choices[0].text });
+  } catch (error) {
+    console.error("error", error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+export default router;
